dbcf44eef5367a141ad4f152f602526d81a971f4
卞成栋 <1793745434@qq.com>
2023-07-06 08:54:43 +0800

    up

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/Test2Controller.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/Test2Controller.java
@@ -1,19 +1,47 @@
 package com.ruoyi.web.controller.system;
 
 
+import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.system.domain.Test;
 import com.ruoyi.system.service.impl.TestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
-public class Test2Controller {
+@RequestMapping("test")
+public class Test2Controller extends BaseController {
+
     @Autowired
     private TestServiceImpl testService;
-    @GetMapping("/test")
-    public AjaxResult getList(){
-        return AjaxResult.success(testService.getById(1)) ;
+
+    @GetMapping("/list")
+    public TableDataInfo getList() {
+        startPage();
+        return getDataTable(testService.list());
     }
+
+    @PostMapping("/add")
+    public AjaxResult add(@RequestBody Test test) {
+        if (test.getName() != null && test.getAge() != null && test.getWeight() != null && test.getHeight() != null) {
+            return AjaxResult.success(testService.save(test));
+        }
+        return AjaxResult.error("未填写信息，添加失败");
+    }
+
+    @PostMapping("/delete")
+    public AjaxResult delete(@RequestBody List<Integer> ids) {
+        return AjaxResult.success(testService.removeByIds(ids));
+    }
+
+    @PostMapping("/update")
+    public AjaxResult update(@RequestBody Test test) {
+        return AjaxResult.success(testService.updateById(test));
+    }
+
+
 }
